67e32e6c11dcc24f0637a1d430cf51a7c1ffeb14
Om Amar <emailomamar@gmail.com>
2025-05-20 01:43:25 +0530

    updated

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, render_template, request, jsonify
+from flask import session
+from model.predict import predict
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite3"
+# db.init_app(app)
+app.app_context().push()
+app.secret_key = "APtlnuRu04uv"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+# with app.app_context():
+    # db.create_all()
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict1():
+    d = request.get_json()
+    for i in d:
+        try:
+            d[i] = int(d[i])
+        except:
+            pass
+    k = predict(d['carat'],d['cut'],d['clarity'],d['table'],d['x'],d['y'],d['z'])
+    return jsonify({"ans":k}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- model/predict.py
+++ model/predict.py
@@ -1,14 +1,22 @@
 import joblib
 import pandas as pd
 import numpy as np
+import os
 
-ct = joblib.load("column_transformer.pkl")
-model = joblib.load("ridge_best.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ct = joblib.load(os.path.join(BASE_DIR, "column_transformer.pkl"))
+sc = joblib.load(os.path.join(BASE_DIR, "Standard_Scaler.pkl"))
+model = joblib.load(os.path.join(BASE_DIR, "ridge_best.pkl"))
+
 
 def predict(carat,cut,clarity,table,x,y,z):
-    df = pd.DataFrame({'carat':[carat],'table':[table],'x':[x],'y':[y],'z':[z],'Volume':[x*y*z],'cut':[cut],'clarity':[clarity]})
+    df = pd.DataFrame({'carat':[carat],'table':[table],'x':[x],'y':[y],'z':[z],'Volume':[x*y*z],'cut':[cut],'clarity':[clarity],'id':[0],'depth':[0],'color':['F']})
 
     X = ct.transform(df)
+    X.drop(['remainder__color'],axis=1,inplace=True)
+
+    X = sc.transform(X)
 
     return model.predict(X)[0]
 
